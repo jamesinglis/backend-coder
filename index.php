788037853ecaa357610fa6ed4743f34afa627ac6
James Inglis <james@webcode.no>
2016-03-24 00:36:06 +0100

    James adds his application :)

--- index.php
+++ index.php
@@ -48,3 +48,128 @@ class Maksimer_Backend_Coder    {
     }
 }
 
+
+interface Wannabe_Maksimer_Backend_Coder
+{
+
+    public function looking_for_a_challinging_job();
+
+    public function knows_php_to_fingertips();
+
+    public function really_eager_to_learn();
+
+    public function has_junior_potential();
+
+    public function can_wear_a_lumberjack_shirt_every_friday();
+
+    public function will_code_for_food();
+
+    public function will_work_hard_when_required();
+
+    public function sees_producing_great_code_as_a_reward();
+
+    public function has_good_coffee_brewing_morality();
+
+    public function know_what_wordpress_is();
+
+    public function know_what_a_wordpress_hook_is();
+
+    public function wants_to_work_in_bergen_or_stavanger();
+
+    public function email_cv_and_short_summary($email_address, $subject, $body);
+}
+
+class James_Inglis_Backend_Coder implements Wannabe_Maksimer_Backend_Coder
+{
+
+    protected $caffeinated = false;
+
+    protected $full_name = 'James Inglis';
+
+    public function __construct(array $inputs)
+    {
+        if (in_array('coffee', $inputs)) {
+            $this->caffeinated = true;
+        }
+    }
+
+    public function get_full_name()
+    {
+        return $this->full_name;
+    }
+
+    public function looking_for_a_challinging_job()
+    {
+        return false; // Already have a great one with @WebcodeAS!
+    }
+
+    public function knows_php_to_fingertips()
+    {
+        return true; // A bit more advanced than a script kiddie
+    }
+
+    public function really_eager_to_learn()
+    {
+        return true; // Any developer who isn't will be obsolete in 2 years!
+    }
+
+    public function has_junior_potential()
+    {
+        return true; // I'd hope so... Senior potential too ;)
+    }
+
+    public function can_wear_a_lumberjack_shirt_every_friday()
+    {
+        return true; // Can, yes; look good, that's a different story
+    }
+
+    public function will_code_for_food()
+    {
+        return true; // Yep. Salary is good too!
+    }
+
+    public function will_work_hard_when_required()
+    {
+        if (!$this->caffeinated) {
+            return false;
+        }
+        return true; // Can I automate it? :)
+    }
+
+    public function sees_producing_great_code_as_a_reward()
+    {
+        return true; // Code smells, be gone!
+    }
+
+    public function has_good_coffee_brewing_morality()
+    {
+        return true; // Coffee is my life - who would I be to deprive the next person of that?
+    }
+
+    public function know_what_wordpress_is()
+    {
+        return true; // It's a blog, right?
+    }
+
+    public function know_what_a_wordpress_hook_is()
+    {
+        return true; // I use them hourly...
+    }
+
+    public function wants_to_work_in_bergen_or_stavanger()
+    {
+        return false; // Sorry, Sørlandet all the way!
+    }
+
+    public function email_cv_and_short_summary($email_address, $subject, $body)
+    {
+        mail($email_address, $subject, $body, 'From: hello@jamesinglis.no');
+    }
+}
+
+try {
+    $applicant = new James_Inglis_Backend_Coder(array('coffee'));
+    $position = new Maksimer_Backend_Coder($applicant);
+} catch (Exception $e) {
+    echo sprintf('Applicant %s is not suitable for this position but loves your job advertisement!', $applicant->get_full_name());
+}
